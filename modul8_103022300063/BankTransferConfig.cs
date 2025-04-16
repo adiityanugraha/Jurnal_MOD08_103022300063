46a451cde37fb52581df415e404568f28519b86b
adiityanugraha <150656990+adiityanugraha@users.noreply.github.com>
2025-04-16 08:13:23 +0700

    Add files via upload
    aplikasi bank transfer dengan runtime configuration

---
+++ modul8_103022300063/BankTransferConfig.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul8_103022300063
+{
+
+    class Transfer
+    {
+        public int threshold { get; set; }
+        public int low_fee { get; set; }
+        public int high_fee { get; set; }
+    }
+
+    class Confirmation
+    {
+        public string en { get; set; }
+        public string id { get; set; }
+    }
+
+    class BankTransferConfig
+    {
+        public string lang { get; set; }
+        public Transfer transfer { get; set; }
+        public Confirmation confirmation { get; set; }
+        public List<string> methods { get; set; } = ["RTO, (real-time)", "SKN", "RTGS", "BI", "FAST"];
+
+        string path = Path.GetFullPath("@D: modul8_103022300063\bank_transfer_config.json");
+
+        public BankTransferConfig LoadConf()
+        {
+            if (!File.Exists(path))
+            {
+                var defaultSetting = new BankTransferConfig();
+                SaveNewConfig(defaultSetting);
+                return defaultSetting;
+            }
+
+            string json = File.ReadAllText(path);
+            return JsonSerializer.Deserialize<BankTransferConfig>(json);
+        }
+
+        public void SaveNewConfig(BankTransferConfig config)
+        {
+            var json = JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(path, json);
+        }
+
+        public void GantiBahasa()
+        {
+            if (lang.Equals("en"))
+            {
+                lang = "id";
+            }
+            else if (lang.Equals("id"))
+            {
+                lang = "en";
+            }
+        }
+    }
+}
---
+++ modul8_103022300063/Program.cs
@@ -0,0 +1,44 @@
+﻿using modul8_103022300063;
+using System.Text.Json;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BankTransferConfig config = new BankTransferConfig().LoadConf();
+
+        Console.WriteLine("Ubah bahasa saat ini? (y/n)");
+            string input = Console.ReadLine();
+
+        if (input.ToLower() == "y")
+        {
+            config.GantiBahasa();
+            config.SaveNewConfig(config);
+            Console.WriteLine("Bahasa telah diubah ke: " + config.lang);
+        }
+        else
+        {
+            Console.WriteLine("Bahasa tetap: " + config.lang);
+        }
+
+        if (config.lang.Equals("en"))
+        {
+            Console.WriteLine("Please insert the amount of money to transfer: ");
+        }
+        else
+        {
+            Console.WriteLine("Silakan masukkan jumlah uang yang akan ditransfer: ");
+        }
+        int amount = Convert.ToInt32(Console.ReadLine());
+
+        if (amount <= config.transfer.threshold || amount == config.transfer.threshold)
+        {
+            Console.WriteLine("Transfer fee: " + config.transfer.low_fee);
+        }
+        else if (amount >= config.transfer.threshold)
+        {
+            Console.WriteLine("Transfer fee: " + config.transfer.high_fee);
+        }
+    }
+}
